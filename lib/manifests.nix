6c2df3b49eec705d5d2718f4192b24055aa90a6b
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-10-27 14:05:41 -0300

    fix: automatically consider the latest stable version

--- lib/manifests.nix
+++ lib/manifests.nix
@@ -1,7 +1,20 @@
-{...}: let
+{lib, ...}: let
   rawManifests = import ../manifests/stable/default.nix;
   versions = rawManifests.versions;
-  latest = "1.18.4";
+
+  # Automatically determine the latest stable version (excluding RC versions)
+  latest = let
+    stableVersions = lib.attrNames (
+      lib.filterAttrs
+      (version: _: !(lib.hasInfix "-rc" version))
+      versions
+    );
+    sortedVersions =
+      lib.sort
+      (a: b: lib.versionOlder a b)
+      stableVersions;
+  in
+    lib.last sortedVersions;
 in {
   inherit versions latest;
 }
